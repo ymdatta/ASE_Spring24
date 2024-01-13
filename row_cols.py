bdef0d336f06b154f53b74a3a885e7a84de3c9ec
Harini Ravi <harinirh5@gmail.com>
2024-01-13 16:03:54 -0500

    initial analysis of row and cols classes

---
+++ row_cols.py
@@ -0,0 +1,21 @@
+'''
+ROW AND COLS ANALYSIS
+overall structure:
+
+class ROW:
+    def __init__(self,t):
+        self.cells = t
+    
+    # def d2h() ignored as per hw2 instruction
+
+    # def likes() ignored as per hw2 instruction
+
+    # def like() ignored as per hw2 instruction
+
+class COLS:
+    def __init__(self, row):
+        
+
+
+
+'''
