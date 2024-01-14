94ed86c3a52f7877a0692cfa5e62e827d1996ccc
Jay Vishaal J <jayvishaal17065@cse.ssn.edu.in>
2024-01-14 00:50:50 -0500

    Added Other required Methods in Data.py

--- src/Data.py
+++ src/Data.py
@@ -16,3 +16,28 @@ class Data:
             self.rows.append(self.cols.add(row))
         else:
             self.cols = Cols(row)
+
+    def mid(self, cols=None, u=None):
+        u = [col.mid() for col in (cols or self.cols.all)]
+        return Row(u)
+
+    def div(self, cols=None, u=None):
+        u = [col.div() for col in (cols or self.cols.all)]
+        return Row(u)
+
+    def small(self, u=None):
+        u = [col.small() for col in (self.cols.all)]
+        return Row(u)
+
+    def stats(self, cols, callback, ndivs, u):
+        ## skipping as per comments in hw2.md
+        pass
+        # u = {".N": len(self.rows)}
+        # for col in getattr(self.cols, cols or "y"):
+        #     u[col.txt] =
+
+    def clone(self, rows=None, new=None):
+        new = Data([self.cols.names])
+        for row in (rows or []):
+            new.add(row)
+        return new
