6fdea6322b1e4930e4969f2291fb68432a62f566
harini237 <60064395+harini237@users.noreply.github.com>
2024-01-15 19:14:17 -0500

    Update and rename row_cols.py to src/row_cols.py

--- src/row_cols.py
+++ src/row_cols.py
@@ -20,7 +20,7 @@ class COLS:
         self.names = row.cells
         for at, txt in row.cells.items():
             col = NUM(txt, at) if re.search("^[A-Z]", txt) else SYM(txt, at)
-            all[len(all) + 1, col]
+            all[len(all) + 1] = col
             if !txt.endswith("X"):
                 if txt.endswsith("!"):
                     self.klass = col
