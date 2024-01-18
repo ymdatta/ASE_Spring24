f209ff724adc5fecc5e9f10646daf93313e0aad7
Jay Vishaal J <jayvishaal17065@cse.ssn.edu.in>
2024-01-17 23:29:34 -0500

    Task 1 Added Percentage Values

--- hw3/w3/task1/code/diabetes_count.py
+++ hw3/w3/task1/code/diabetes_count.py
@@ -4,7 +4,7 @@ d = {}
 d['negative'] = 0
 d['positive'] = 0
 i = 0
-with open("diabetes.csv", mode='r') as file:
+with open("hw3/w3/data/diabetes.csv", mode='r') as file:
     csvfile = csv.reader(file)
     for x in csvfile:
         if i > 0 :
@@ -14,3 +14,5 @@ with open("diabetes.csv", mode='r') as file:
                 d['positive'] = d['positive'] + 1
         i = i + 1
 print(d)
+print("Negative Class  =",d['negative']/(d['negative'] + d['positive'])*100, "%")
+print("Positive Class  =",d['positive']/(d['negative'] + d['positive'])*100, "%")
